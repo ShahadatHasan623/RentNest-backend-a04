183eefa306bd207911046c2377eb454d2ae44515
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-07-09 20:00:22 +0600

    added prisma user table created

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import app from "./app";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 const PORT = config.port;
 const main = async () => {
   try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully");
+    console.log();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.log("Error starting the server:", error);
+    await prisma.$disconnect();
+    process.exit(1);
   }
 };
-main()
+main();
